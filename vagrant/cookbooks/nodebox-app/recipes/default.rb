a58a2d98a71b47e59bb50bd45ba8d3b6f0311732
slloyd <steve@repeatingbeats.com>
2011-01-31 22:33:19 -0800

    Got npm and node working with proper permissions. Added ability to install node modules via an array in the Vagrantfile.

--- vagrant/cookbooks/nodebox-app/recipes/default.rb
+++ vagrant/cookbooks/nodebox-app/recipes/default.rb
@@ -1,4 +1,8 @@
 
+class Chef::Resource
+  include NodeHelpers
+end
+
 apt_update = execute "update apt" do
   user "root"
   command "apt-get update"
@@ -15,4 +19,19 @@ end
 
 include_recipe "nodejs"
 include_recipe "nodejs::npm"
+
+# change ownership of /usr/local so npm plays nicely with node
+bash "give vagrant ownership of /usr/local" do
+  user "root"
+  code "chown -R vagrant /usr/local"
+  not_if { chowned_usrlocal? }
+end
+
+# install our required npm modules
+node[:node_modules].each do | node_module |
+  bash "npm install #{node_module}" do
+    user node[:node_user]
+    code "npm install #{node_module}"
+  end
+end
 
