bab3b9ee8f0268305d8989d02eea4b2422525b72
slloyd <steve@repeatingbeats.com>
2011-02-05 19:15:06 -0800

    Nodebox is working. Upstart daemonizes the node process. Monit retarts node after crashes.

--- app.js
+++ app.js
@@ -2,6 +2,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.end('Hello World, I Am A NodeBox\n');
 }).listen(80);
 console.log("Server running at localhost:80");
--- vagrant/cookbooks/nodebox-app/recipes/default.rb
+++ vagrant/cookbooks/nodebox-app/recipes/default.rb
@@ -6,7 +6,7 @@ apt_update = execute "update apt" do
 end
 apt_update.run_action(:run)
 
-%w{vim curl man-db git-core upstart}.each do | pkg |
+%w{vim curl man-db git-core upstart monit}.each do | pkg |
   install_package = package pkg do
     action :nothing
   end
@@ -32,10 +32,36 @@ node[:node_modules].each do | node_module |
   end
 end
 
+execute "start upstart" do
+  user "root"
+  command "start #{node[:appname]}"
+  action :nothing
+end
+
+execute "start monit" do
+  user "root"
+  command "monit -d 60 -c /etc/monit/monitrc"
+  action :nothing
+end
+
 template "/etc/init/#{node[:appname]}.conf" do
   source "upstart.erb"
   owner "root"
   group "root"
   mode 0755
+  notifies :run, resources(:execute => "start upstart")
 end
 
+template "/etc/monit/monitrc" do
+  source "monit.erb"
+  owner "root"
+  group "root"
+  mode 0700
+  notifies :run, resources(:execute => "start monit")
+end
+
+=begin
+  TODO:
+  - create user for node and run node with reduced privileges
+  - reload on code changes in development mode
+=end
