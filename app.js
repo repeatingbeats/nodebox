7f2c347f22b13a342e8b7bffcf2280e3067773b5
slloyd <steve@repeatingbeats.com>
2011-02-13 22:39:24 -0800

    Rework so that nodebox is configured via json in app root folder.

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 
-var ENV_PRODUCTION = 'production',
-    ENV_DEVELOPMENT = 'development'
-;
+var fs = require('fs');
 
-var nodeboxEnv = ENV_PRODUCTION;
-var nodeboxPort = (nodeboxEnv == ENV_PRODUCTION) ? 8080 : 80;
+console.log(process.cwd());
+// Read nodebox configuration, then start the app
+fs.readFile('/var/www/nodebox-example/nodebox.json', function (err, data) {
+  if (err) throw err;
+  var config = JSON.parse(data);
+  var env = config.environment;
+  var port = config.app_port || env == "production" ? 8080 : 80;
+  listen(env, port);
+});
 
-
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World, I Am A NodeBox\n');
-}).listen(nodeboxPort);
-console.log("Server running at localhost:" + nodeboxPort);
+function listen(environment, port) {
+  var http = require('http');
+  http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World, I Am A NodeBox\n');
+  }).listen(port);
+  console.log("Server listening on port:" + port);
+}
 
