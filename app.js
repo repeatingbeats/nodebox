79590b83011a8a865cdc6381791f92e5d5501113
slloyd <steve@repeatingbeats.com>
2011-02-16 21:20:55 -0800

    Example app shouldn't care about the node process's cwd.

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
 
-var fs = require('fs');
+var fs = require('fs'),
+    http = require('http')
+;
 
-console.log(process.cwd());
-// Read nodebox configuration, then start the app
-fs.readFile('/var/www/nodebox-example/nodebox.json', function (err, data) {
-  if (err) throw err;
-  var config = JSON.parse(data);
-  var env = config.environment;
-  var port = config.app_port || env == "production" ? 8080 : 80;
-  listen(env, port);
-});
+var config = JSON.parse(fs.readFileSync(__dirname + '/nodebox.json'));
+var env = config.environment;
+var port = config.app_port || env == "production" ? 8080: 80;
 
-function listen(environment, port) {
-  var http = require('http');
-  http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World, I Am A NodeBox\n');
-  }).listen(port);
-  console.log("Server listening on port:" + port);
-}
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World, I Am A NodeBox\n');
+}).listen(port);
+console.log("Server listening on port: " + port);
 
